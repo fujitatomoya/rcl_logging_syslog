a64023e2b0badc0daf0a4c2babba32a8173af7a0
Tomoya Fujita <Tomoya.Fujita@sony.com>
2024-09-25 12:53:25 -0700

    add logging file and contents check via rsyslog test conf.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- test/test_logging_interface.cpp
+++ test/test_logging_interface.cpp
@@ -12,11 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <chrono>
 #include <filesystem>
 #include <fstream>
 #include <random>
 #include <sstream>
 #include <string>
+#include <thread>
 
 #include "gmock/gmock.h"
 
@@ -42,6 +44,20 @@ static constexpr int logger_levels[] =
   RCUTILS_LOG_SEVERITY_ERROR,
   RCUTILS_LOG_SEVERITY_FATAL,
 };
+
+static void write_something()
+{
+  for (int level : logger_levels) {
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_set_logger_level(nullptr, level));
+    std::stringstream ss;
+    ss << "Message of severity " << level << " at level " << level;
+    rcl_logging_external_log(level, nullptr, ss.str().c_str());
+  }
+
+  // give flush interval period for rsyslog (default 1 sec flush interval for file system)
+  // ref: https://www.rsyslog.com/doc/configuration/modules/omfile.html#flushinterval
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+}
 
 // This is a helper class that resets an environment
 // variable when leaving scope
@@ -100,6 +116,82 @@ private:
   }
 };
 
+class LoggingTest : public ::testing::Test
+{
+public:
+  void SetUp()
+  {
+    // test log files will be created under /var/log/rcl_logging_syslog/xxx.
+    // rcl_logging_syslog folder will be created by rsyslog when it is writing.
+    var_log_ = std::filesystem::path("/var/log");
+    ASSERT_TRUE(std::filesystem::exists(var_log_));
+    test_log_dir_ = var_log_ / "rcl_logging_syslog";
+  }
+
+  void TearDown()
+  {
+    if (std::filesystem::remove_all(test_log_dir_) == 0) {
+      std::cerr << "Failed to remove " << test_log_dir_.string() << " directory\n";
+    }
+  }
+
+  bool remove_test_folder()
+  {
+    if (std::filesystem::remove_all(test_log_dir_) == 0) {
+      std::cerr << "Failed to remove " << test_log_dir_.string() << " directory\n";
+      return false;
+    }
+    return true;
+  }
+
+  std::filesystem::path find_single_log(const char * prefix)
+  {
+    std::string expected_prefix = get_expected_log_prefix(prefix);
+
+    std::filesystem::path found;
+    std::filesystem::file_time_type found_last_write;
+    for (const std::filesystem::directory_entry & dir_entry :
+      std::filesystem::directory_iterator{test_log_dir_})
+    {
+      // If the start of the filename matches the expected_prefix, and this is the newest file
+      // starting with that prefix, hold onto it to return later.
+      if (dir_entry.path().filename().string().rfind(expected_prefix, 0) == 0) {
+        if (found.string().empty() || dir_entry.last_write_time() > found_last_write) {
+          found = dir_entry.path();
+          found_last_write = dir_entry.last_write_time();
+          // Even though we found the file, we have to keep looking in case there
+          // is another file with the same prefix but a newer timestamp.
+        }
+      }
+    }
+
+    return found;
+  }
+
+  rcutils_allocator_t allocator;
+
+private:
+  std::string get_expected_log_prefix(const char * name)
+  {
+    char * exe_name;
+    if (name == nullptr || name[0] == '\0') {
+      exe_name = rcutils_get_executable_name(allocator);
+    } else {
+      exe_name = rcutils_strdup(name, allocator);
+    }
+    if (nullptr == exe_name) {
+      throw std::runtime_error("Failed to determine executable name");
+    }
+    std::stringstream prefix;
+    prefix << exe_name << "_" << rcutils_get_pid() << "_";
+    allocator.deallocate(exe_name, allocator.state);
+    return prefix.str();
+  }
+
+  std::filesystem::path test_log_dir_;
+  std::filesystem::path var_log_;
+};
+
 TEST_F(AllocatorTest, init_invalid)
 {
   EXPECT_EQ(
@@ -135,4 +227,183 @@ TEST_F(AllocatorTest, init_valid)
     RCL_LOGGING_RET_OK,
     rcl_logging_external_initialize(nullptr, "config_file", allocator));
   rcutils_reset_error();
+}
+
+TEST_F(LoggingTest, log_file_name_prefix)
+{
+  std::string log_file_path;
+  // executable name in default
+  {
+    ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize(nullptr, nullptr, allocator));
+    write_something();
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_shutdown());
+    EXPECT_NO_THROW(log_file_path = find_single_log(nullptr).string());
+    EXPECT_TRUE(remove_test_folder());
+  }
+  // falls back to executable name if not nullptr, but empty
+  {
+    ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize("", nullptr, allocator));
+    write_something();
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_shutdown());
+    EXPECT_NO_THROW(log_file_path = find_single_log(nullptr).string());
+    EXPECT_TRUE(remove_test_folder());
+  }
+  // specified by user application
+  {
+    ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize("logger", nullptr, allocator));
+    write_something();
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_shutdown());
+    EXPECT_NO_THROW(log_file_path = find_single_log("logger").string());
+    EXPECT_TRUE(remove_test_folder());
+  }
+}
+
+TEST_F(LoggingTest, use_different_facilities)
+{
+  RestoreEnvVar env_var("RCL_LOGGING_SYSLOG_FACILITY");
+
+  {
+    // default facility, LOG_LOCAL1
+    ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize(nullptr, nullptr, allocator));
+
+    std::stringstream expected_log;
+    for (int level : logger_levels) {
+      EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_set_logger_level(nullptr, level));
+
+      for (int severity : logger_levels) {
+        std::stringstream ss;
+        ss << "Message of severity " << severity << " at level " << level;
+        rcl_logging_external_log(severity, nullptr, ss.str().c_str());
+
+        if (severity >= level) {
+          expected_log << ss.str() << std::endl;
+        } else if (severity == 0 && level == 10) {
+          // This is a special case - not sure what the right behavior is
+          expected_log << ss.str() << std::endl;
+        }
+      }
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_shutdown());
+
+    std::string log_file_path = find_single_log(nullptr).string();
+    std::ifstream log_file(log_file_path);
+    std::stringstream actual_log;
+    actual_log << log_file.rdbuf();
+    EXPECT_EQ(
+      expected_log.str(),
+      actual_log.str()) << "Unexpected log contents in " << log_file_path;
+    EXPECT_TRUE(remove_test_folder());
+  }
+
+  {
+    // facility LOG_LOCAL2
+    rcpputils::set_env_var("RCL_LOGGING_SYSLOG_FACILITY", "LOG_LOCAL2");
+    ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize(nullptr, nullptr, allocator));
+
+    std::stringstream expected_log;
+    for (int level : logger_levels) {
+      EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_set_logger_level(nullptr, level));
+
+      for (int severity : logger_levels) {
+        std::stringstream ss;
+        ss << "Message of severity " << severity << " at level " << level;
+        rcl_logging_external_log(severity, nullptr, ss.str().c_str());
+
+        if (severity >= level) {
+          expected_log << ss.str() << std::endl;
+        } else if (severity == 0 && level == 10) {
+          // This is a special case - not sure what the right behavior is
+          expected_log << ss.str() << std::endl;
+        }
+      }
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_shutdown());
+
+    std::string log_file_path = find_single_log(nullptr).string();
+    std::ifstream log_file(log_file_path);
+    std::stringstream actual_log;
+    actual_log << log_file.rdbuf();
+    EXPECT_EQ(
+      expected_log.str(),
+      actual_log.str()) << "Unexpected log contents in " << log_file_path;
+    EXPECT_TRUE(remove_test_folder());
+  }
+
+  {
+    // facility LOG_LOCAL3
+    rcpputils::set_env_var("RCL_LOGGING_SYSLOG_FACILITY", "LOG_LOCAL3");
+    ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize(nullptr, nullptr, allocator));
+
+    std::stringstream expected_log;
+    for (int level : logger_levels) {
+      EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_set_logger_level(nullptr, level));
+
+      for (int severity : logger_levels) {
+        std::stringstream ss;
+        ss << "Message of severity " << severity << " at level " << level;
+        rcl_logging_external_log(severity, nullptr, ss.str().c_str());
+
+        if (severity >= level) {
+          expected_log << ss.str() << std::endl;
+        } else if (severity == 0 && level == 10) {
+          // This is a special case - not sure what the right behavior is
+          expected_log << ss.str() << std::endl;
+        }
+      }
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_shutdown());
+
+    std::string log_file_path = find_single_log(nullptr).string();
+    std::ifstream log_file(log_file_path);
+    std::stringstream actual_log;
+    actual_log << log_file.rdbuf();
+    EXPECT_EQ(
+      expected_log.str(),
+      actual_log.str()) << "Unexpected log contents in " << log_file_path;
+    EXPECT_TRUE(remove_test_folder());
+  }
+}
+
+TEST_F(LoggingTest, full_cycle)
+{
+  ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize(nullptr, nullptr, allocator));
+
+  // Make sure we can call initialize more than once
+  ASSERT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_initialize(nullptr, nullptr, allocator));
+
+  std::stringstream expected_log;
+  for (int level : logger_levels) {
+    EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_set_logger_level(nullptr, level));
+
+    for (int severity : logger_levels) {
+      std::stringstream ss;
+      ss << "Message of severity " << severity << " at level " << level;
+      rcl_logging_external_log(severity, nullptr, ss.str().c_str());
+
+      if (severity >= level) {
+        expected_log << ss.str() << std::endl;
+      } else if (severity == 0 && level == 10) {
+        // This is a special case - not sure what the right behavior is
+        expected_log << ss.str() << std::endl;
+      }
+    }
+  }
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  EXPECT_EQ(RCL_LOGGING_RET_OK, rcl_logging_external_shutdown());
+
+  std::string log_file_path = find_single_log(nullptr).string();
+  std::ifstream log_file(log_file_path);
+  std::stringstream actual_log;
+  actual_log << log_file.rdbuf();
+  EXPECT_EQ(
+    expected_log.str(),
+    actual_log.str()) << "Unexpected log contents in " << log_file_path;
+  EXPECT_TRUE(remove_test_folder());
 }
